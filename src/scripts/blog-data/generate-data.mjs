f5bb43f1cb5bcec9f0ea399256bd10336ca2594d
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-04 22:54:58 +0800

    turbo.json added

--- src/scripts/blog-data/generate-data.mjs
+++ src/scripts/blog-data/generate-data.mjs
@@ -2,6 +2,7 @@ import { writeFileSync } from "node:fs";
 import generateBlogData from "../../next-data/generators/blogData.mjs";
 
 const blogData = await generateBlogData();
+console.log("IAM WAORKING");
 
 writeFileSync(
   new URL(`../../../src/public/blog-data.json`, import.meta.url),
