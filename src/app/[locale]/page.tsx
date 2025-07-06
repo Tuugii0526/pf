e789f01ae4ef6f217ec7448257a83ff915098282
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-07 00:20:13 +0800

    lang

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,5 @@
+import { Greeting } from "@/components/Greeting";
 import { Languages } from "@/lib/constant";
-import { dm_serif_display, roboto } from "@/lib/fonts/fonts";
-
 export const generateStaticParams = async () => {
   const routes = await Promise.all(
     Languages.map((locale: string) => ({ locale }))
@@ -8,15 +7,5 @@ export const generateStaticParams = async () => {
   return routes;
 };
 export default async function Home() {
-  return (
-    <div className="font-bold">
-      <p className={`text-[45px] ${dm_serif_display.className}`}>
-        👨‍💻 Сайн уу !
-      </p>
-      <p className={`${roboto.className} `}>
-        Бодлын урсгал ~~~, туршлага , программийн мэдлэг , сонссоноо та бүхэндээ
-        хуваалцах болно . Тавтай морил <span className="text-3xl">🤗</span>
-      </p>
-    </div>
-  );
+  return <Greeting />;
 }
--- src/app/[locale]/works/page.tsx
+++ src/app/[locale]/works/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div className="font-bold"> ...Coming soon ...</div>;
+  return <div className="w-[90%] font-bold"> ...Coming soon ...</div>;
 }
