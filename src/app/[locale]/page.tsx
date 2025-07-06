d932a3e0f40d5cd028847aef52255d13f981a59e
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-07 00:06:40 +0800

    homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import { Languages } from "@/lib/constant";
+import { dm_serif_display, roboto } from "@/lib/fonts/fonts";
 
 export const generateStaticParams = async () => {
   const routes = await Promise.all(
@@ -7,5 +8,15 @@ export const generateStaticParams = async () => {
   return routes;
 };
 export default async function Home() {
-  return <div className="font-bold">...Coming soon...</div>;
+  return (
+    <div className="font-bold">
+      <p className={`text-[45px] ${dm_serif_display.className}`}>
+        👨‍💻 Сайн уу !
+      </p>
+      <p className={`${roboto.className} `}>
+        Бодлын урсгал ~~~, туршлага , программийн мэдлэг , сонссоноо та бүхэндээ
+        хуваалцах болно . Тавтай морил <span className="text-3xl">🤗</span>
+      </p>
+    </div>
+  );
 }
