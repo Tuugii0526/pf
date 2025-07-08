e4e80e911c0b552364cccbf802b5f939c2315fcd
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-08 20:12:22 +0800

    incremental static site

--- src/app/[locale]/blog/[...path]/page.tsx
+++ src/app/[locale]/blog/[...path]/page.tsx
@@ -8,6 +8,8 @@ import { CategoriesT } from "@/lib/types/categories";
 import { languageCodes } from "@/lib/types/i18n";
 import { getBlog, getLangPathParams, getMetadata } from "@/lib/utils";
 import { MDXRemote } from "next-mdx-remote/rsc";
+// export const dynamic = "force-static";
+// export const revalidate = 300;
 export const generateStaticParams = async () => {
   return getLangPathParams();
 };
