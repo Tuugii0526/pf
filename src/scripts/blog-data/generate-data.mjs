d8cfae5def61ffa2aa12101a6342518e0ccd4f3d
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-14 01:58:05 +0800

    script working check

--- src/scripts/blog-data/generate-data.mjs
+++ src/scripts/blog-data/generate-data.mjs
@@ -1,6 +1,6 @@
 import { writeFileSync } from "node:fs";
 import generateBlogData from "../../next-data/generators/blogData.mjs";
-
+console.log("SCRIPT IS WORKING");
 const blogData = await generateBlogData();
 writeFileSync(
   new URL(`../../../src/data/blog-data.json`, import.meta.url),
