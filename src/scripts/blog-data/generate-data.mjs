16534224ccf00e20319da4590669b5b39cf3c585
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-14 02:08:56 +0800

    cwd

--- src/scripts/blog-data/generate-data.mjs
+++ src/scripts/blog-data/generate-data.mjs
@@ -1,9 +1,11 @@
 import { writeFileSync } from "node:fs";
 import generateBlogData from "../../next-data/generators/blogData.mjs";
+console.log("current working dir:", process.cwd());
+console.log("current file:", import.meta.url);
 try {
   const blogData = await generateBlogData();
   writeFileSync("./src/data/blog-data.json", JSON.stringify(blogData), "utf8");
 } catch (error) {
-  console.lor(error);
+  console.log(error);
   console.log("error is inside generateing");
 }
