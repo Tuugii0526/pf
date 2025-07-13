72840ff64790f22604c9ec5a1204f9ed78a48e49
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-14 02:06:21 +0800

    ./src

--- src/scripts/blog-data/generate-data.mjs
+++ src/scripts/blog-data/generate-data.mjs
@@ -2,11 +2,8 @@ import { writeFileSync } from "node:fs";
 import generateBlogData from "../../next-data/generators/blogData.mjs";
 try {
   const blogData = await generateBlogData();
-  writeFileSync(
-    new URL(`../../../src/data/blog-data.json`, import.meta.url),
-    JSON.stringify(blogData),
-    "utf8"
-  );
+  writeFileSync("./src/data/blog-data.json", JSON.stringify(blogData), "utf8");
 } catch (error) {
+  console.lor(error);
   console.log("error is inside generateing");
 }
