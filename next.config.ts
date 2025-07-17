8dab21e89b55632fd5798400c197b78db1f2137f
Tuugii0526 <namjildorjtuguldur1234@gmail.com>
2025-07-17 11:55:27 +0800

    cloudinary image

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
+  images: {
+    remotePatterns: [new URL("https://res.cloudinary.com/**")],
+  },
 };
 
 const withMDX = createMDX({
